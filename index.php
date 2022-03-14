417d2bed12cf3abed29211cb45bd8949aa80d6dd
craigtrecher <craigtrecher@gmail.com>
2022-03-14 12:57:53 +0800

    3-14-22
    -initial necessary css
    -initial necessary js
    -initial dashboard
    -complete with icons
    -disabled functionality

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <link href="css/fa-css/all.min.css" rel="stylesheet" type="text/css">
+    
+
+</head>
+<body>
+   
+<div> <!-- First Div-->
+    <!-- First UL-->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <div class="sidebar-brand-icon rotate-n-15">
+        </div>
+        <div class="sidebar-brand-text mx-3">K3L POS 2.0</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link text-center" href="#">
+            <span>DASHBOARD</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+            <!-- Empty Content Heading -->
+    </div>
+
+    <!-- Nav Item - Manage Users -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-users">
+            </i>
+            <span>Manage Users</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Update Product -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-wrench">
+            </i>
+            <span>Update Product</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Invoicing -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-file-invoice">
+            </i>
+            <span>Invoicing</span>
+            <span class="badge badge-pill badge-danger" >3</span> 
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Project Manager -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-user">
+            </i>
+            <span>Project Manager</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Post Collection -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-inbox">
+            </i>
+            <span>Post Collection</span>
+            <span class="badge badge-pill badge-danger" >5</span> 
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Nav Item - Project Monitor -->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" 
+            aria-expanded="true" aria-controls="collapseInbox" target="accounts_iframe">
+            <i class="fas fa-fw fa-chart-pie">
+            </i>
+            <span>Project Monitor</span>
+        </a>
+    </li>
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+    </ul><!-- First UL-->
+
+    <!-- End of Sidebar -->
+
+
+
+</div> <!-- First Div-->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+    <script src="js/dataTables.bootstrap4.min.js"></script>
+
+</body>
+</html>
