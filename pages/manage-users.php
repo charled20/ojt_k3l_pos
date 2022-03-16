dab46084a40f667d2200036f03cfe9413cad0bc4
craigtrecher <craigtrecher@gmail.com>
2022-03-17 02:45:37 +0800

    3-17-22 2:45am
    -added prototype/skeleton manage-users.php
    -added working table->pdf exporter

--- pages/manage-users.php
+++ pages/manage-users.php
@@ -7,6 +7,113 @@
     <title>Manage Users</title>
 </head>
 <body>
-    Manage Users
+    <button>EDIT</button>
+    <br>
+    <table>
+        <tr>
+            <th>
+                USERCODE
+            </th>
+            <th>
+                USERNAME
+            </th>
+            <th>
+                P1
+            </th>
+            <th>
+                P2
+            </th>
+            <th>
+                P3
+            </th>
+            <th>
+                P4
+            </th>
+        </tr>
+        <tr>
+            <td>
+                100
+            </td>
+            <td>
+                USER1
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox"checked>
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                102
+            </td>
+            <td>
+                USER2
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                103
+            </td>
+            <td>
+                USER3
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox" >
+            </td>
+        </tr>
+        <tr>
+            <td>
+                104
+            </td>
+            <td>
+                USER4
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+            <td>
+                <input type="checkbox" checked>
+            </td>
+        </tr>
+    </table>
+    PROCESS P1: INVOICING <br>
+    PROCESS P2: PROJECT MANAGER <br>
+    PROCESS P3: POST COLLECTION <br>
+    PROCESS P4: PROJECT MONITOR
 </body>
 </html>
