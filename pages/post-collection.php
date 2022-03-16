5905733ea54137722d5e333b59ae43b6fb580abd
craigtrecher <craigtrecher@gmail.com>
2022-03-17 02:22:16 +0800

    3-17-22 2:21am
    -added skeleton/prototype of post-collection
    -added skeleton/prototype of project-manager

--- pages/post-collection.php
+++ pages/post-collection.php
@@ -7,6 +7,33 @@
     <title>Post Collection</title>
 </head>
 <body>
-Post Collection
+Generate <input type="text"> &nbsp;&nbsp;&nbsp;Project ID <input type="text">Delivery Invoice <input type="text">
+<br>
+
+<table style="border: 1px solid black;">
+    <tr>
+        <th style="border: 1px solid black;">
+        <div>
+            Project Details <br>
+            <input type="text"> <button>SEARCH</button>
+        </div>
+        </th>
+
+        <th style="border: 1px solid black;">
+        <div>
+            Delivery Invoice <br>
+            Invoice No. &nbsp; &nbsp; TOTAL AMOUNT
+        </div>
+        </th>
+    </tr>
+</table>
+
+PAYMENT DETAILS <br>
+AMOUNT&nbsp;<input type="text">CHECK NUMBER&nbsp; <input type="text"> <br>
+PARTICULARS &nbsp; <input type="text">
+
+<button>SAVE</button> <button>PRINT PREVIEW</button> <button>NEW TRANSACTION</button> <button>CLEAR</button> &nbsp;&nbsp;&nbsp;<button>EXIT</button>
+
+
 </body>
 </html>
--- pages/project-manager.php
+++ pages/project-manager.php
@@ -7,6 +7,33 @@
     <title>Project Manager</title>
 </head>
 <body>
-    Project Manager
+    <div>
+        <button>Project Code</button> <input type="text">&nbsp;&nbsp;&nbsp;Project Cost&nbsp;<input type="text">
+    </div>
+    Project Name&nbsp;&nbsp; <input type="text">
+    <br>
+    Location&nbsp;&nbsp; <input type="text">
+    <br>
+    Duration in Days <input type="text"> &nbsp;&nbsp;&nbsp; START DATE <select name="" id=""><option value="">Option 1</option><option value="">Option 2</option></select>
+    <br>
+    <input type="text" placeholder="SEARCH">&nbsp;&nbsp;<button>F2-SAVE</button><button>F3-EDIT</button><button>CLEAR</button><button>F10-EXIT</button>
+    <br>
+    <br>
+    <table style="border: 1px solid black;">
+        <tr>
+            <th>CODE</th>
+            <th>PROJECT NAME</th>
+            <th>LOCATION</th>
+            <th>COST</th>
+        </tr>
+        <tr>
+            <td>107</td>
+            <td>OJT Project</td>
+            <td>BXU</td>
+            <td>800.00</td>
+        </tr>
+    </table>
+
+    <button>PROJECT MONITOR</button>
 </body>
 </html>
